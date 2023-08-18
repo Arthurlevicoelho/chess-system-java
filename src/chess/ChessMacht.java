9cbfc79acaa7c884073b9288dd20560bf07865af
Arthur Levi frota Coelho <arthurlevicoelho@hotmail.com>
2023-08-18 15:59:52 -0300

    BoardException and defensive programming

--- src/chess/ChessMacht.java
+++ src/chess/ChessMacht.java
@@ -23,8 +23,8 @@ public class ChessMacht {
 	}
 	private void initialSetup() {
 		board.placePiece(new Rook(board,Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board,Color.BLACK), new Position(0,4));
-		board.placePiece(new King(board,Color.WHITE), new Position(0,4));
+		board.placePiece(new King(board,Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board,Color.WHITE), new Position(7,4));
 
 		
 	}
